8f5fad56e798b1dd2f8d13c33d8430e247eab5bc
claudiobzrr <claudio.bezerra1998@gmail.com>
2025-07-07 21:26:54 -0300

    style: format code for consistency and readability in env.ts and server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,23 @@
-import { fastify } from 'fastify'
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
-import { fastifyCors } from '@fastify/cors'
-import { env } from './env.ts'
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { env } from "./env.ts";
 
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.register(fastifyCors, {
+	origin: "http://localhost:5173",
+});
 
-app.register( fastifyCors,{
-    origin:'http://localhost:5173',
-})
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
+app.get("/health", () => "OK");
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
-
-
-app.get('/health',() => 'OK')
-
-app.listen({port: env.PORT}).then(() => console.log(`Server is running on port ${env.PORT}`))
+app
+	.listen({ port: env.PORT })
+	.then(() => console.log(`Server is running on port ${env.PORT}`));
